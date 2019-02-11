c7013bbe6e90ddd5fcb852a920ba5924f4f973ec
Hannes <h.geissler@schille.com>
2019-02-11 16:23:25 +0100

    Carousel + Container

---
+++ src/components/Carousel.vue
@@ -0,0 +1,53 @@
+<template>
+  <v-carousel height="400">
+    <v-carousel-item
+      v-for="(item, i) in items"
+      :key="i"
+      :src="item.src"
+    >
+      <v-container fill-height>
+        <v-layout column fill-height justify-center align-center>
+          <v-flex xs12>
+            <span v-if="i == 0" class="imgheadline">Portrait</span>
+            <span v-if="i == 1" class="imgheadline">Hochzeit</span>
+            <span v-if="i == 2" class="imgheadline">Pärchen</span>
+            <span v-if="i == 3" class="imgheadline">Akt</span>
+          </v-flex>
+        </v-layout>
+      </v-container>
+    </v-carousel-item>
+  </v-carousel>
+</template>
+
+<style scoped>
+  .imgheadline {
+    font: 32px Comfortaa;
+    line-height: 400;
+    text-shadow: 1px 1px 1px #333;
+    opacity: 0.6;
+  }
+</style>
+
+
+<script>
+export default {
+  data() {
+    return {
+      items: [
+        {
+          src: 'https://images.unsplash.com/photo-1531251445707-1f000e1e87d0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1000&q=80'
+        },
+        {
+          src: 'https://images.unsplash.com/photo-1521543269800-27e08780ced7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1000&q=80'
+        },
+        {
+          src: 'https://images.unsplash.com/photo-1505765052322-75804bb2e5f1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1000&q=80'
+        },
+        {
+          src: 'https://images.unsplash.com/photo-1519668752166-ebdbfe986afd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1000&q=80'
+        }
+      ]
+    }
+  }
+}
+</script>
